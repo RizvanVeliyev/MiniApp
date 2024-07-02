e9581ce9e07e00a065850c9b093218e317a59d1b
RizvanVeliyev <veliyevrizvan250@gmail.com>
2024-07-02 16:41:08 +0400

    3 methods in Med service

--- MiniApp/Services/MedicineService.cs
+++ MiniApp/Services/MedicineService.cs
@@ -45,6 +45,62 @@ namespace MiniApp.Services
             throw new NotFoundException("verilen id ile derman tapilmadi!");
         }
 
+        public Medicine GetMedicineByName(string name)
+        {
+            foreach (var medicine in DB.medicines)
+            {
+                if (medicine.Name == name)
+                {
+                    return medicine;
+                }
+            }
+            throw new NotFoundException("verilen ad ile derman tapilmadi!");
+        }
+
+
+
+        public Medicine[] GetMedicineByCategory(int categoryId)
+        {
+            int count = 0;
+            foreach (var medicine in DB.medicines)
+            {
+                if (medicine.CategoryId == categoryId)
+                {
+                    count++;
+                }
+            }
+
+            if (count == 0)
+            {
+                throw new NotFoundException("verilen kateqoriya id-si ile derman yoxdur!");
+            }
+
+            Medicine[] medicines = new Medicine[count];
+            int index = 0;
+            foreach (var medicine in DB.medicines)
+            {
+                if (medicine.CategoryId == categoryId)
+                {
+                    medicines[index++] = medicine;
+                }
+            }
+
+            return medicines;
+        }
+
+
+        public void UpdateMedicine(int id, Medicine updatedMedicine)
+        {
+            for (int i = 0; i < DB.medicines.Length; i++)
+            {
+                if (DB.medicines[i].Id == id)
+                {
+                    DB.medicines[i] = updatedMedicine;
+                    return;
+                }
+            }
+            throw new NotFoundException("verilen id ile derman tapilmadi!");
+        }
 
 
 
