d05dd59f1295ab671c2320a7a38ba8da3a6a2b11
RizvanVeliyev <veliyevrizvan250@gmail.com>
2024-07-02 16:04:16 +0400

    Add project files.

---
+++ MiniApp/Models/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniApp.Models
+{
+    public abstract class BaseEntity
+    {
+        private static int _id = 1;
+        public int Id { get; private set; }
+
+        protected BaseEntity()
+        {
+            Id = _id++;
+        }
+    }
+}
---
+++ MiniApp/Models/DB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniApp.Models
+{
+    public static class DB
+    {
+        public static User[] users;
+        public static Category[] categories;
+        public static Medicine[] medicines;
+
+        static DB()
+        {
+            users = new User[0];
+            categories = new Category[0];
+            medicines = new Medicine[0];
+        }
+    }
+}
